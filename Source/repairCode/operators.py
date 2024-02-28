3f8c1df335f126c90353a4a5b823906f6de1bd30
Xiang Ma <xiangm@iastate.edu>
2024-02-28 15:16:02 -0600

    Update operators.py

--- Source/repairCode/operators.py
+++ Source/repairCode/operators.py
@@ -7,20 +7,43 @@ from pyggi.base import BaseOperator
 
 ## Implement new operators here
 
-class ReplaceOperator(BaseOperator):
-    def __init__(self, name, tag, quantum_gates):
-        super(ReplaceOperator, self).__init__(name)
-        self.tag = tag
-        self.quantum_gates = quantum_gates
+class QGateReplacement(StmtReplacement):
+    def __init__(self, target, ingredient, target_tag):
+        super(QGateReplacement, self).__init__(target, ingredient)
+        self.target_tag = target_tag
 
-    def apply(self, program):
-        tree = program.get_tree()
+    def apply(self, program, new_contents, modification_points):
+        engine = program.engines[self.target[0]]
+        return engine.do_replace(program, self, new_contents, modification_points)
 
-        # Find the statement with the tag
-        statement = tree.findall(tag)
+    def do_replace(self, program, new_contents, modification_points):
+        target_content = new_contents[self.target[0]]
+        ingredient_content = new_contents[self.ingredient[0]]
 
-        # Replace the current quantum gate with a random selection
-        statement.text = random.choice(self.quantum_gates)
+        # Parse the XML content
+        target_tree = etree.fromstring(target_content)
+
+        # Find all elements with the target tag
+        elements = target_tree.findall(".//{}".format(self.target_tag))
+
+        if elements:
+            # Choose a random element to replace
+            element_to_replace = random.choice(elements)
+
+            # Parse the XML content of the ingredient
+            ingredient_tree = etree.fromstring(ingredient_content)
+
+            # Replace the chosen element with the entire content of the ingredient
+            parent = element_to_replace.getparent()
+            parent.replace(element_to_replace, ingredient_tree)
+
+            # Serialize the modified XML back to a string
+            new_target_content = etree.tostring(target_tree, pretty_print=True).decode('utf-8')
+
+            return new_target_content
+
+        return target_content
+
 
 
 class AddOperator(BaseOperator):
