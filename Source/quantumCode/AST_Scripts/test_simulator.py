dbb89201307a079af1f57b9f87041baf3af027aa
liyili2 <liyili2@illinois.edu>
2024-04-05 12:40:01 -0500

    update.

--- Source/quantumCode/AST_Scripts/test_simulator.py
+++ Source/quantumCode/AST_Scripts/test_simulator.py
@@ -3,6 +3,11 @@
 from collections import ChainMap
 from simulator import *
 from XMLExpLexer import XMLExpLexer
+from antlr4 import *
+from SpecExpParser import *
+from SpecExpVisitor import *
+from SpecGen import *
+from VarCollector import *
 
 ####################
 ## What is input ###
@@ -25,6 +30,24 @@ from XMLExpLexer import XMLExpLexer
 # Write a series of tests HERE
 class Test_Simulator(object):
 
+    def test_result(self, spec: SpecExpParser.ProgramContext, st: dict):
+        specVisitor = SpecGen(st)
+        return specVisitor.visitProgram(spec).getResult()
+
+
+    def test_pattern(self, spec: SpecExpParser.ProgramContext):
+        varVisitor = VarCollector()
+        st = varVisitor.visitProgram(spec)
+        # need to use random testing generator to generate values in st
+        return test_result(spec, st)
+
+    #test_pattern above is rewritten for the usage of pytest.
+    #pytest will call test_pattern to test a spec
+    #it can also call test_result by inputing a state, and then get
+    #the resulting state and check
+    # the result state is a list of nat numbers
+    #user are responsible to turn the nat numbers to Nvals, then check if
+    #the quantum component satisfies with the output
     def test_init(self):
         # //We first turn x array to QFT type, and we apply SR gate to rotate the phase of x for 2 pi i * (1/2^10). It will make sense if 10 < rmax, RQFT is the inverse of QFT.
         str = "<pexp gate = 'QFT' > <id> x </id>  <vexp> 0 </vexp> </pexp> <pexp gate = 'SR' > < vexp> 10 </vexp> <id> x </id> </pexp> <pexp gate = 'RQFT' > <id> x </id>  <vexp> 0 </vexp> </pexp> "
