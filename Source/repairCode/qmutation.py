f1bf1ef66e5d3c02121b94270cbf4895cc8b289f
Michael C. Gerten <mgerten@knox.edu>
2024-05-13 14:13:12 -0500

    Import check in QMutation

--- Source/repairCode/qmutation.py
+++ Source/repairCode/qmutation.py
@@ -1,5 +1,6 @@
 import random
 from jmetal.core.operator import Mutation
+from jmetal.util.ckecking import Check
 from .qpatch import QPatch
 
 """
@@ -40,6 +41,27 @@ class QMutation(Mutation[QPatch]):
 
         Change an item in the item list (remove and replace in same location)
         """
+        Check.that(type(solution) is QPatch, "Solution type invalid")
+
+        lp = len(solution.edit_list)
+        for j in range(lp):
+            rand = random.random()
+            if rand <= self.probability:
+                rnd = random.random()
+                if lp > 1 and rnd < 0.33:
+                    if j < len(solution.edit_list):
+                        solution.remove(j)
+                    j -= 1
+                    lp -= 1
+                elif lp == 0 or rnd < 0.66:
+                    edit_operator = random.choice(self.prg.operators)
+                    solution.add(edit_operator.create(self.prg))
+                else:
+                    edit_operator = random.choice(self.prg.operators)
+                    if j < len(solution.edit_list):
+                        solution.edit_list[j] = edit_operator.create(self.prg)
+
+        return solution
         return solution
 
     def get_name(self):
