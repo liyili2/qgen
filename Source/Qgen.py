63d74199171c3064ad65e54bba68a7fadbc09dd4
Michael C Gerten <mcgerten@gmail.com>
2024-05-14 13:23:01 -0500

    Added a QResult object to store information relate to the quantum result

--- Source/Qgen.py
+++ Source/Qgen.py
@@ -10,13 +10,12 @@ from pyggi.tree import StmtReplacement, StmtInsertion, StmtDeletion # Default Py
 from jmetal.algorithm.singleobjective import GeneticAlgorithm
 from jmetal.operator import BinaryTournamentSelection
 from jmetal.util.termination_criterion import StoppingByEvaluations
-from jmetal.core.quality_indicator import FitnessValue, QualityIndicator
 # For modified program + repair
 from repairCode.qprogram import QProgram
 from repairCode.qproblem import QProblem
-from repairCode.qpatch import QPatch
-from repairCode.mutation import NullMutation, PyGGiMutation
+from repairCode.mutation import PyGGiMutation
 from repairCode.crossover import PyGGiCrossover
+# Custom Operators
 from repairCode.operators import QGateReplacement, QGateInsertion, QGateDeletion
 
 class MyXmlEngine(XmlEngine):
@@ -40,16 +39,16 @@ class MyXmlEngine(XmlEngine):
 if __name__ == "__main__":
     print("Starting")
     parser = argparse.ArgumentParser(description='PYGGI Bug Repair Example')
-    parser.add_argument('--project_path', type=str, default='Benchmark/Triangle')
-    parser.add_argument('--algorithm', type=str, default='ga')
-    parser.add_argument('--epoch', type=int, default=1, help='total epoch(default: 1)')
-    parser.add_argument('--iter', type=int, default=8, help='total iterations per epoch(default: 100)')
-    parser.add_argument('--pop', type=int, default=8, help='population size(default: 10)')
-    parser.add_argument('--mutation', type=float, default=1, help='mutation rate(default: 0.1)')
-    parser.add_argument('--crossover', type=float, default=1, help='crossover rate(default: 0.9)')
-    parser.add_argument('--sel', type=str, default='tournament', help='selection operator(default: tournament)')
-    parser.add_argument('--tags', type=str, default='[]', help='XML tags (default: [])')
-    parser.add_argument('--operators', type=str, default='[]', help='Operators (default: [])')
+    parser.add_argument('--project_path', type=str,   default='Benchmark/Triangle')
+    parser.add_argument('--algorithm',    type=str,   default='ga')
+    parser.add_argument('--epoch',        type=int,   default=1,            help='total epoch(default: 1)')
+    parser.add_argument('--iter',         type=int,   default=8,            help='total iterations per epoch(default: 100)')
+    parser.add_argument('--pop',          type=int,   default=8,            help='population size(default: 10)')
+    parser.add_argument('--mutation',     type=float, default=1,            help='mutation rate(default: 0.1)')
+    parser.add_argument('--crossover',    type=float, default=1,            help='crossover rate(default: 0.9)')
+    parser.add_argument('--sel',          type=str,   default='tournament', help='selection operator(default: tournament)')
+    parser.add_argument('--tags',         type=str,   default='[]',         help='XML tags (default: [])')
+    parser.add_argument('--operators',    type=str,   default='[]',         help='Operators (default: [])')
     args = parser.parse_args()
 
     # Make a Program
