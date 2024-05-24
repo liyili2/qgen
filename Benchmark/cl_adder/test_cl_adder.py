8daeafd78635bb2ef0c18c2814867a94cb9f9407
Michael C. Gerten <mgerten@knox.edu>
2024-05-24 09:16:03 -0500

    Removed unneeded triangle code.

--- Benchmark/cl_adder/test_cl_adder.py
+++ Benchmark/cl_adder/test_cl_adder.py
@@ -2,37 +2,10 @@ import time
 import pytest
 from antlr4 import InputStream, CommonTokenStream
 
-from Benchmark.Triangle.triangle import TriangleType, classify_triangle # this might not be correct
-from quantumCode.AST_Scripts.XMLExpLexer import XMLExpLexer
-from quantumCode.AST_Scripts.XMLExpParser import XMLExpParser
-from quantumCode.AST_Scripts.simulator import calBin, Coq_nval, Simulator, calInt
-
-
-def check_classification(triangles, expected_result):
-    for triangle in triangles:
-        assert classify_triangle(*triangle) == expected_result
-
-
-def test_invalid_triangles():
-    triangles = [(1, 2, 9), (1, 9, 2), (2, 1, 9), (2, 9, 1), (9, 1, 2),
-                 (9, 2, 1), (1, 1, -1), (1, -1, 1), (-1, 1, 1)]
-    check_classification(triangles, TriangleType.INVALID)
-
-
-def test_equalateral_triangles():
-    triangles = [(1, 1, 1), (100, 100, 100), (99, 99, 99)]
-    check_classification(triangles, TriangleType.EQUALATERAL)
-
-
-def test_isoceles_triangles():
-    triangles = [(100, 90, 90), (90, 100, 90), (90, 90, 100), (3, 3, 2), (3, 2, 3), (2, 3, 3)]
-
-    check_classification(triangles, TriangleType.ISOCELES)
-
-
-def test_scalene_triangles():
-    triangles = [(5, 4, 3), (5, 3, 4), (4, 5, 3), (4, 3, 5), (3, 5, 4)]
-    check_classification(triangles, TriangleType.SCALENE)
+#from Benchmark.Triangle.triangle import TriangleType, classify_triangle # this might not be correct
+from Source.quantumCode.AST_Scripts.XMLExpLexer import XMLExpLexer
+from Source.quantumCode.AST_Scripts.XMLExpParser import XMLExpParser
+from Source.quantumCode.AST_Scripts.simulator import calBin, Coq_nval, Simulator, calInt
 
 
 def test_init(self):
