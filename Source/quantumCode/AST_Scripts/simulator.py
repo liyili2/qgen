67e66f785564e987a1d98de0946ffdd0ea4f527b
liyili2 <liyili2@illinois.edu>
2024-05-29 12:47:18 -0500

    update.

--- Source/quantumCode/AST_Scripts/simulator.py
+++ Source/quantumCode/AST_Scripts/simulator.py
@@ -178,7 +178,7 @@ class Simulator(XMLExpVisitor):
                     ctx.exppair(i).exp().accept(self)
                     return
             else:
-                y = ctx.exppair(i).vexp().vexp().accept(self)
+                y = ctx.exppair(i).vexp().vexp(0).accept(self)
                 self.st.update({y: v - 1})
                 ctx.exppair(i).exp().accept(self)
             i += 1
