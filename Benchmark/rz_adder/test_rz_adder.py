e14504efdf779d5c95eb93877785f6de8c1f2dd6
pavani <pavanironanki55.com>
2024-06-07 02:21:26 -0500

    Added rz_adder tests

--- Benchmark/rz_adder/test_rz_adder.py
+++ Benchmark/rz_adder/test_rz_adder.py
@@ -6,37 +6,8 @@ from Source.quantumCode.AST_Scripts.XMLExpParser import XMLExpParser
 from Source.quantumCode.AST_Scripts.simulator import CoqNVal, Simulator, bit_array_to_int, to_binary_arr
 
 
-#def check_classification(triangles, expected_result):
-#    for triangle in triangles:
- #       assert classify_triangle(*triangle) == expected_result
-
-
-#def test_invalid_triangles():
- #   triangles = [(1, 2, 9), (1, 9, 2), (2, 1, 9), (2, 9, 1), (9, 1, 2),
-     #            (9, 2, 1), (1, 1, -1), (1, -1, 1), (-1, 1, 1)]
-  #  check_classification(triangles, TriangleType.INVALID)
-
-
-# test_equalateral_triangles():
-  #  triangles = [(1, 1, 1), (100, 100, 100), (99, 99, 99)]
- #   check_classification(triangles, TriangleType.EQUALATERAL)
-
-
-#def test_isoceles_triangles():
-#    triangles = [(100, 90, 90), (90, 100, 90), (90, 90, 100), (3, 3, 2), (3, 2, 3), (2, 3, 3)]
-
-#    check_classification(triangles, TriangleType.ISOCELES)
-
-
-#def test_scalene_triangles():
-#    triangles = [(5, 4, 3), (5, 3, 4), (4, 5, 3), (4, 3, 5), (3, 5, 4)]
-#    check_classification(triangles, TriangleType.SCALENE)
-
-
-# for the first step, the fitness is the percentage of correctness. How many test cases a program run correctly.
-# the correctness is defined as array, x, y and c, the input is (x) with a constant m, and the output is (x+m)
-
-def test_init():
+# Test function to initialize and run the rz_adder simulation
+def run_rz_adder_test(num_qubits,val,addend):
     with open("Benchmark/rz_adder/rz_adder_good.xml", 'r') as f:
         str = f.read()
     i_stream = InputStream(str)
@@ -46,9 +17,9 @@ def test_init():
     tree = parser.program()
     print(tree.toStringTree(recog=parser))
 
-    num_qubits = 16  # Number of Qubits
-    val = 100  # init value
-    addend = 10
+    # num_qubits = 16  # Number of Qubits
+    # val = 100  # init value
+    # addend = 10
     val_array = to_binary_arr(val, num_qubits)  # conver value to array
     state = dict({"x": [CoqNVal(val_array, 0)], "na": num_qubits,
                   "m": addend})  # initial a chainMap having variable "x" to be 0 (list of False)
@@ -57,8 +28,25 @@ def test_init():
     y = Simulator(state, environment)  # Environment is same, initial state varies by pyTest
     y.visitProgram(tree)
     new_state = y.get_state()
-    assert (110 == bit_array_to_int(new_state.get('x')[0].getBits(), num_qubits))
+    return bit_array_to_int(new_state.get('x')[0].getBits(), num_qubits)
 
+def test_basic_addition():
+    assert run_rz_adder_test(16, 5, 4) == 9
+
+def test_carry_propagation():
+    assert run_rz_adder_test(4, 14, 3) == 1  # 1110 + 0011 = 10001 % 2^4 = 0001
+
+def test_array_size_limit():
+    assert run_rz_adder_test(4, 3, 13) == 0  # 0011 + 1101 = 10000 % 2^4 = 0000
+
+def test_large_numbers():
+    assert run_rz_adder_test(64, 2**32, 2**32) == 2**33  # 2^32 + 2^32 % 2^64 = 0
+
+def test_zero_addend():
+    assert run_rz_adder_test(16, 10, 0) == 10
+
+def test_zero_qubit_array():
+    assert run_rz_adder_test(16, 0, 5) == 5
 
 @pytest.fixture(scope="session", autouse=True)
 def starter(request):
